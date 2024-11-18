cf8e40f6ba3ab2aed4121232c8b5948e4aa2fa15
Luuk Kablan <luckyluuk02@gmail.com>
2024-11-18 12:56:54 +0100

    feat: detail modifications

--- plugins/project_packer/src/index.ts
+++ plugins/project_packer/src/index.ts
@@ -1,6 +1,14 @@
 // @ts-ignore - allow the .register method to be called without error
 Plugin.register('project_packer', {
-  title: 'Project Packer',
-  icon: 'icon.png'
-
+  title: "Project Packer ",
+  author: "Luuk Kablan, Zegevlier",
+  icon: "icon.png",
+  description: "Manage your Minecraft resource pack / project and optimize it!",
+  has_changelog: false,
+  min_version: "4.11.0",
+  max_version: "5.0.0",
+  variant: "both",
+  website: "https://github.com/LuckyLuuk12/Project-Packer",
+  repository: "https://github.com/JannisX11/blockbench-plugins/tree/master/plugins/project_packer",
+  bug_tracker: "https://github.com/LuckyLuuk12/Project-Packer/issues?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen"
 });
