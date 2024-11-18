b0f46638d4f47325a4f3d2b82cb126272963621c
Luuk Kablan <luckyluuk02@gmail.com>
2024-11-18 12:46:23 +0100

    fix: move package.json

---
+++ plugins/project_packer/project_packer.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/*!******************!*\
+  !*** ./index.ts ***!
+  \******************/
+// @ts-ignore - allow the .register method to be called without error
+Plugin.register('project_packer', {
+    title: 'Project Packer',
+    icon: 'icon.png'
+});
+
+/******/ })()
+;
